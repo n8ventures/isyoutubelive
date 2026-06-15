bbe9a95eee4f91f37fcd83bbd958a89f941d69dc
John Nathaniel Calvara <nate@n8ventures.dev>
2026-06-15 11:58:51 +0800

    fix: update params

--- index.d.ts
+++ index.d.ts
@@ -15,6 +15,8 @@ export interface VideoData {
 }
 
 export type ShortData = VideoData;
+export type CheckMode = boolean | 'membersOnly';
+
 
 /**
  * Check if a YouTube channel is currently live.
@@ -25,13 +27,16 @@ export function checkLive(channelID: string): Promise<LiveData>;
 /**
  * Get the latest uploaded video from a YouTube channel.
  * @param channelID - YouTube channel handle (e.g. '@LinusTechTips') or ID.
- * @param public - Whether to include members-only videos in the search. Defaults to true, meaning it will only detect public videos.
+ * @param mode - Whether to include members-only videos in the search. Defaults to true, meaning it will only detect public videos. Use 'membersOnly' if you want Members Only Videos.
  */
-export function checkVideo(channelID: string, public: boolean): Promise<VideoData>;
+export function checkVideo(
+  channelID: string,
+  mode?: CheckMode
+): Promise<VideoData>;
 
 /**
  * Get the latest short from a YouTube channel.
  * @param channelID - YouTube channel handle (e.g. '@LinusTechTips') or ID.
- * @param public - Whether to include members-only shorts in the search. Defaults to true, meaning it will only detect public shorts.
+ * @param mode - Whether to include members-only shorts in the search. Defaults to true, meaning it will only detect public shorts. Use 'membersOnly' if you want Members Only Shorts.
  */
-export function checkShort(channelID: string, public: boolean): Promise<ShortData>;
+export function checkShort(channelID: string, mode?: CheckMode): Promise<ShortData>;
